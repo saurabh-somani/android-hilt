0a31d174e84f34a13ca90d546023f3390c0bba31
Saurabh Somani <50082769+saurabh-somani@users.noreply.github.com>
2021-10-04 19:01:49 +0530

    Created a module for injecting database dependencies.

--- app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/hilt/ui/MainActivity.kt
@@ -22,7 +22,9 @@ import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 /**
  * Main activity of the application.
  *
